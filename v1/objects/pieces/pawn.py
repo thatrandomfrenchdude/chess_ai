c5325318203c1211458aced3ddf34d8a17bb7768
Nicholas Debeurre <debeurren@gmail.com>
2023-08-01 09:10:11 -0700

    refactor en passant

--- v1/objects/pieces/pawn.py
+++ v1/objects/pieces/pawn.py
@@ -14,38 +14,54 @@ class Pawn(Piece):
     def is_starting_position(self):
         return self.y == 1 if self.color == Color.BLACK else self.y == 6
 
+    def en_passant(self, board):
+        ans = []
+
+        # get the x values to the left and right of the pawn
+        l_x = self.x - 1 if self.is_white() else self.x + 1
+        r_x = self.x + 1 if self.is_white() else self.x - 1
+
+        # get the pieces around the pawn
+        l_piece = board.get_piece(l_x, self.y)
+        r_piece = board.get_piece(r_x, self.y)
+
+        if self.en_passant_left(l_piece):
+            ans.append(self.get_move(board, l_x, self.y - 1 if self.is_white() else self.y + 1))
+        if self.en_passant_right(r_piece):
+            ans.append(self.get_move(board, r_x, self.y - 1 if self.is_white() else self.y + 1))
+
+        # assumes board oriented with white along the bottom
+        # return self.en_passant_left(l_piece), self.en_passant_right(r_piece)
+        return ans
+
     # TODO: merge these two functions
-    # TODO: refine color checking?
     # confirmed working as white with left en passant possible
     # check if piece to left of pawn has just moved two from starting position
-    def en_passant_left(self, board):
-        # assumes board oriented with white along the bottom
-        if not board.is_clone:
-            if self.is_white():
-                print("white pawn")
-        piece_left = board.get_piece(self.x - 1, self.y)
-        
-        if piece_left == 0:
+    def en_passant_left(self, piece):
+        # make sure there is a piece to the left
+        if piece == 0:
             return False 
         else:
-            if piece_left.piece_type == Pawn.PIECE_TYPE and \
-                piece_left.color != self.color and \
-                piece_left.just_moved_two:
+            if piece.piece_type == Pawn.PIECE_TYPE and \
+                piece.color is not self.color and \
+                piece.just_moved_two:
                 print("En passant left!")
                 return True
         return False
     
     # check if piece to right of pawn has just moved two from starting position
-    def en_passant_right(self, board):
-        # assumes board oriented with white along the bottom
-        piece_right = board.get_piece(self.x + 1, self.y)
-        
-        if piece_right == 0:
+    def en_passant_right(self, piece):
+        # make sure there is a piece to the right
+        if piece == 0:
             return False 
-        elif piece_right.piece_type == Pawn.PIECE_TYPE and piece_right.color != self.color and piece_right.just_moved_two:
-            print("En passant right!")
-            return True
+        else:
+            if piece.piece_type == Pawn.PIECE_TYPE and \
+                piece.color is not self.color and \
+                piece.just_moved_two:
+                print("En passant right!")
+                return True
         return False
+
 
     def get_possible_moves(self, board):
         moves = []
@@ -67,19 +83,24 @@ class Pawn(Piece):
                 moves.append(self.get_move(board, self.x + dx, self.y + direction))
 
         # append en passant if applicable
+        moves.extend(self.en_passant(board))
         # TODO: check that these coordinates are correct for left and right
-        if self.color == Color.WHITE:
-            if self.en_passant_left(board):
-                moves.append(self.get_move(board, self.x - 1, self.y - 1))
-            if self.en_passant_right(board):
-                moves.append(self.get_move(board, self.x + 1, self.y - 1))
-        else:
-            if self.en_passant_left(board):
-                moves.append(self.get_move(board, self.x + 1, self.y + 1))
-            if self.en_passant_right(board):
-                moves.append(self.get_move(board, self.x - 1, self.y + 1))
+        # if self.is_white():
+        #     # get sides
+        #     left, right = self.en_passant(board)
+            
+        #     if left:
+        #         moves.append(self.get_move(board, self.x - 1, self.y - 1))
+        #     if right:
+        #         moves.append(self.get_move(board, self.x + 1, self.y - 1))
+        # else:
+        #     if self.en_passant_left(board):
+        #         moves.append(self.get_move(board, self.x + 1, self.y + 1))
+        #     if self.en_passant_right(board):
+        #         moves.append(self.get_move(board, self.x - 1, self.y + 1))
 
         return self.remove_null_from_list(moves)
 
     def clone(self):
         return Pawn(self.x, self.y, self.color)
+    
